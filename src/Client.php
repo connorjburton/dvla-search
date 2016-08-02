22bc1628fff19346f9972a5a5e85dafff1fb0e51
Connor Burton <connorjburton@gmail.com>
2016-08-02 19:39:59 +0100

    VehicleSearchClient done + test

--- src/Client.php
+++ src/Client.php
@@ -3,26 +3,37 @@ namespace DVLASearch\SDK;
 
 class Client {
 	const ENDPOINT = 'https://dvlasearch.appspot.com/%s?apikey=%s&licencePlate=%s';
-	private $type = '';
-	private $key = '';
 
-	public function __construct(string $key, string $type) {
-		$this->key = $key;
+	private $type = '';
+	private $key = 'DvlaSearchDemoAccount';
+
+	public function __construct(string $key, string $type)
+	{
+		if($key) $this->key = $key;
 		$this->type = $type;
 	}
 
-	private function parse(string $plate): string {
+	private function parse(string $plate): string
+	{
 		if(!$plate) {
-			throw new Exception('Empty $plate given in DVLASearch\SDK::parseUrl');
+			throw new Exception('Empty $plate given');
 		} else {
-			return vsprintf($this::ENDPOINT, $this->type, $this->key, $plate);
+			return sprintf($this::ENDPOINT, $this->type, $this->key, $plate);
 		}
 	}
 
-	protected function query(string $plate) {
+	protected function query(string $plate)
+	{
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $this->parse($plate));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		$this->parse($plate);
+		curl_setopt_array($ch, [
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_URL => $this->parse($plate)
+		]);
+
+		if($result = curl_exec($ch)) {
+			return json_decode($result);
+		} else {
+			throw new Exception('cURL error (' . curl_errno($ch) . '): ' . curl_error($ch));
+		}
 	}
 }
--- src/VehicleSearchClient.php
+++ src/VehicleSearchClient.php
@@ -4,11 +4,13 @@ namespace DVLASearch\SDK;
 class VehicleSearchClient extends Client {
 	const TYPE = 'DvlaSearch';
 
-	public function __construct(string $key) {
-		parent::construct($key, $this::TYPE);
+	public function __construct(string $key = '')
+	{
+		parent::__construct($key, $this::TYPE);
 	}
 
-	public function get(string $plate) {
+	public function get(string $plate)
+	{
 		return $this->query($plate);
 	}
 }
