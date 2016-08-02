d6be51e138e336a3e47b03d8633d1c244ca29bfc
Connor Burton <connorjburton@gmail.com>
2016-08-02 19:49:56 +0100

    Fixing code climate issues

--- src/Client.php
+++ src/Client.php
@@ -15,25 +15,21 @@ class Client {
 
 	private function parse(string $plate): string
 	{
-		if(!$plate) {
-			throw new Exception('Empty $plate given');
-		} else {
-			return sprintf($this::ENDPOINT, $this->type, $this->key, $plate);
-		}
+		if(!$plate) throw new Exception('Empty $plate given');
+		
+		return sprintf($this::ENDPOINT, $this->type, $this->key, $plate);
 	}
 
 	protected function query(string $plate)
 	{
-		$ch = curl_init();
-		curl_setopt_array($ch, [
+		$curl = curl_init();
+		curl_setopt_array($curl, [
 			CURLOPT_RETURNTRANSFER => 1,
 			CURLOPT_URL => $this->parse($plate)
 		]);
 
-		if($result = curl_exec($ch)) {
-			return json_decode($result);
-		} else {
-			throw new Exception('cURL error (' . curl_errno($ch) . '): ' . curl_error($ch));
-		}
+		if(!$result = curl_exec($curl)) throw new Exception('cURL error (' . curl_errno($curl) . '): ' . curl_error($curl));
+		
+		return json_decode($result);
 	}
 }
